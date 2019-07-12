c5255e894e4e008af25ddc6e1d73b297b018db75
Atsukin <atsuki.tamura.2740@gmail.com>
2019-07-12 22:46:18 +0900

    アラートメッセージが表示されるよう

--- task_app/app/controllers/tasks_controller.rb
+++ task_app/app/controllers/tasks_controller.rb
@@ -30,7 +30,7 @@ class TasksController < ApplicationController
   def destroy
     task = Task.find(params[:id])
     task.destroy
-    redirect_to tasks_url
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
   end
 
   private
